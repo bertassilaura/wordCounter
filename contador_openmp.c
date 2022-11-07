369f66a254bc946e2a64894b52dcf9d736025f56
Laura Bertassi <bertassilaura@gmail.com>
2022-11-06 21:42:39 -0300

    openmp

--- contador_openmp.c
+++ contador_openmp.c
@@ -1,116 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <omp.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <sys/time.h>
+#include <omp.h>
+#define OMP_NUM_THREADS 19
 
-int num_words = 30;
-int qtd_word[30];
-char keywords[50][100] = {
-    "jesus",
-    "deus",
-    "temor",
-    "senhor",
-    "pastor",
-    "não tema",
-    "amigo",
-    "montanha",
-    "rei",
-    "desculpa",
-    "dia",
-    "pecado",
-    "tempo",
-    "mora",
-    "amo",
-    "serpente",
-    "homem",
-    "ele",
-    "vai",
-    "decepcionado",
-    "eu",
-    "vida",
-    "rosa",
-    "livro",
-    "interessante",
-    "batismo",
-    "mundo",
-    "flor",
-    "estrela",
-    "noite",
-};
+int ocorrencias_palavras_chave[19];
+char palavras_chave[50][100] = {"death", "that", "never", "soul", "distressful", "divine", "time", "god", "inferno", "make", "escape",
+                                "spirits", "lamentations", "blessed", "manifest", "angelical", "and", "canto", "lord"};
+int qtd_palavras = 19;
 
-int counter(char *line, char *word)
+char *texto(char *arquivo)
 {
-
-    int qtd = 0;
-    int equals = 0;
-
-    for (int i = 0; i < strlen(line); i++)
-    {
-        if (line[i] == word[equals])
-            ++equals;
-        else
-            equals = 0;
-        if (equals == strlen(word))
-        {
-            ++qtd;
-            equals = 0;
-        }
-    }
-    return qtd;
+    FILE *file = fopen(arquivo, "rb");                                                // abre o arquivo
+    fseek(file, 0, SEEK_END);                                                         // cursor avança 0, começa no final.
+    int tam_texto = ftell(file);                                                      // tamanho do inicio do file até o cursor
+    fseek(file, 0, SEEK_SET);                                                         // volta o cursor para o inicio do file
+    unsigned char *data = (unsigned char *)malloc(tam_texto * sizeof(unsigned char)); // cria buffer para armazenar o conteúdo do arquivo
+    fread(data, sizeof(unsigned char), tam_texto, file);                              // le o arquivo para data
+    fclose(file);
+    return data;
 }
 
-void main()
+int num_ocorrencias(char *linha, char *palavra)
 {
-    char *buffer = 0;
-    long size;
+    int cont = 0;
+    int igual = 0;
+
+    for (int i = 0; i < strlen(linha); i++)
+    {
+        if (linha[i] == palavra[igual])
+            ++igual;
+        else
+            igual = 0;
+        if (igual == strlen(palavra))
+        {
+            ++cont;
+            igual = 0;
+        }
+    }
+    return cont;
+}
+
+int main()
+{
 
     struct timeval t1, t2;
     gettimeofday(&t1, NULL);
 
-    FILE *file = fopen("/home/gbl/Projects/paralela/biblia-em-txt-lowercase.txt", "rb");
-
-    if (file)
-    {
-        fseek(file, 0, SEEK_END);
-        size = ftell(file);
-        fseek(file, 0, SEEK_SET);
-        buffer = (char *)malloc(size);
-        if (buffer)
-        {
-            fread(buffer, 1, size, file);
-        }
-        fclose(file);
-    }
-    printf("%s\n", "1");
-
-    if (buffer)
-    {
-        omp_set_num_threads(2);
+    char *text = texto("divinacomedia.txt");
 
 #pragma omp parallel for
-
-        for (int i = 0; i < num_words; i++)
-            qtd_word[i] = counter(buffer, keywords[i]);
-
-        for (int j = 0; j < num_words; j++)
-            printf("%s - %d vezes.\n\n", keywords[j], qtd_word[j]);
-
-        gettimeofday(&t2, NULL);
+    for (int i = 0; i < qtd_palavras; i++)
+    {
+        ocorrencias_palavras_chave[i] = num_ocorrencias(text, palavras_chave[i]); // qtd de ocorrencias da palavra chave procurada
     }
 
+    for (int j = 0; j < qtd_palavras; j++)
+    {
+        printf("%s - %d vez(es).\n\n", palavras_chave[j], ocorrencias_palavras_chave[j]);
+    }
+
+    gettimeofday(&t2, NULL);
     double t_total = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1000000.0);
-    int min = t_total / 60;
-    int h, m, s;
+    printf("tempo total = %f\n", t_total);
 
-    h = (t_total / 3600);
-
-    m = (t_total - (3600 * h)) / 60;
-
-    s = (t_total - (3600 * h) - (m * 60));
-
-    printf("H:M:S - %d:%d:%d\n", h, m, s);
-
-    printf("Tempo em segundos: %f ms\n", t_total);
-    printf("Tempo em minutos: %d min\n\n", min);
+    return 0;
 }
